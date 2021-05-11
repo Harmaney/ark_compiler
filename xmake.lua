0eb21b63dba9ba44b43bace0507284a8b0c0cfde
magic-star <1361915800@qq.com>
2021-05-11 21:04:30 +0800

    123

--- xmake.lua
+++ xmake.lua
@@ -8,6 +8,7 @@ target("ark")
     -- add_cxxflags("-fsanitize=undefined")
     set_kind("binary")
     add_headerfiles("src/*.h")
+    -- fuck you
     add_files("src/*.cpp")
     add_packages("spdlog")
 
