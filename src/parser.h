21a359cbf6e17ba036ac5a72b50dd31ebf3b8321
13306330378 <82038764+13306330378@users.noreply.github.com>
2021-05-16 19:50:04 +0800

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -6,5 +6,21 @@
 using namespace std;
 
 void init();
+struct GrammarTreeNode {
+    string raw, type, parserSymbol;
+    int row, column;
+    uint64_t ID;
+    std::any prop;
 
+    vector<GrammarTreeNode*> son;
+    GrammarTreeNode(string raw, string type, string parserSymbol, int row,
+        int column, uint64_t ID)
+        : raw(raw),
+        type(type),
+        parserSymbol(parserSymbol),
+        row(row),
+        column(column),
+        ID(ID),
+        son{} {}
+};
 GlobalAST* parser_work(string file_name);
