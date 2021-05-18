94ebe098918e110a569bf12bf307f27c9ed131e1
magic-star <1361915800@qq.com>
2021-05-18 20:47:51 +0800

    bin_oct_dev_hex

---
+++ files/testcase/bin_err.pas
@@ -0,0 +1,10 @@
+program Hello;
+var i,j,k,p:longint;
+begin
+    i:=123;
+    j:=%1021;
+    k:=&1234;
+    p:=$12f;
+    write(i+j);
+    write(k+p);
+end.
---
+++ files/testcase/bin_oct_dec_hex.pas
@@ -0,0 +1,10 @@
+program Hello;
+var i,j,k,p:longint;
+begin
+    i:=123;
+    j:=%1001;
+    k:=&1234;
+    p:=$12f;
+    write(i+j);
+    write(k+p);
+end.
