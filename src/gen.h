a2c590ad1700616a1f74e7a6c2ee5803e1a735df
Kanari <iovo7c@gmail.com>
2021-08-18 21:35:30 +0800

    refactor: remove strange function

--- src/gen.h
+++ src/gen.h
@@ -38,45 +38,37 @@ class FunctionAST;
 
 struct VariableDescriptor;
 
+using Value=VariableDescriptor;
 class ADispatcher {
    public:
-    virtual void gen_global_begin(GlobalAST *ast) = 0;
-    virtual void gen_global_end(GlobalAST *ast) = 0;
+    virtual Value gen_global(GlobalAST *ast) = 0;
 
-    virtual void gen_array_type_decl(ArrayTypeDeclAST *ast) = 0;
-    virtual void gen_basic_type(BasicTypeAST *ast) = 0;
-    virtual void gen_pointer_type_decl(PointerTypeDeclAST *ast) = 0;
-    virtual void gen_type_def(TypeDefAST *ast) = 0;
+    virtual Value gen_array_type_decl(ArrayTypeDeclAST *ast) = 0;
+    virtual Value gen_basic_type(BasicTypeAST *ast) = 0;
+    virtual Value gen_pointer_type_decl(PointerTypeDeclAST *ast) = 0;
+    virtual Value gen_type_def(TypeDefAST *ast) = 0;
 
-    virtual void gen_number_expr(NumberExprAST *ast) = 0;
-    virtual void gen_string_expr(StringExprAST *ast) = 0;
-    virtual void gen_char_expr(CharExprAST *ast) = 0;
-    virtual void gen_variable_expr(VariableExprAST *ast) = 0;
-    virtual void gen_return(ReturnAST *ast) = 0;
-    virtual void gen_unary_expr(UnaryExprAST *ast) = 0;
-    virtual void gen_binary_expr(BinaryExprAST *ast) = 0;
-    virtual void gen_call_expr(CallExprAST *ast) = 0;
+    virtual Value gen_number_expr(NumberExprAST *ast) = 0;
+    virtual Value gen_string_expr(StringExprAST *ast) = 0;
+    virtual Value gen_char_expr(CharExprAST *ast) = 0;
+    virtual Value gen_variable_expr(VariableExprAST *ast) = 0;
+    virtual Value gen_return(ReturnAST *ast) = 0;
+    virtual Value gen_unary_expr(UnaryExprAST *ast) = 0;
+    virtual Value gen_binary_expr(BinaryExprAST *ast) = 0;
+    virtual Value gen_call_expr(CallExprAST *ast) = 0;
 
-    /// 只生产if，不包含block
-    virtual void gen_if_statement_begin(IfStatementAST *ast) = 0;
-    virtual void gen_if_statement_else(IfStatementAST *ast) = 0;
-    virtual void gen_if_statement_end(IfStatementAST *ast) = 0;
-    /// 只生产while，不包含block
-    virtual void gen_while_statement_begin(WhileStatementAST *ast) = 0;
-    virtual void gen_while_statement_end(WhileStatementAST *ast) = 0;
-    /// 只生产for，不包含block
-    virtual void gen_for_statement_begin(ForStatementAST *ast) = 0;
-    virtual void gen_for_statement_end(ForStatementAST *ast) = 0;
+    virtual Value gen_if_statement(IfStatementAST *ast) = 0;
+    virtual Value gen_while_statement(WhileStatementAST *ast) = 0;
+    virtual Value gen_for_statement(ForStatementAST *ast) = 0;
 
-    virtual void gen_function(FunctionAST *ast) = 0;
-    virtual void gen_function_signature(FunctionSignatureAST *ast) = 0;
+    virtual Value gen_function(FunctionAST *ast) = 0;
+    virtual Value gen_function_signature(FunctionSignatureAST *ast) = 0;
 
-    virtual void gen_struct(StructDeclAST *ast) = 0;
+    virtual Value gen_struct(StructDeclAST *ast) = 0;
 
-    virtual void gen_block_begin(BlockAST *ast) = 0;
-    virtual void gen_block_end(BlockAST *ast) = 0;
+    virtual Value gen_block(BlockAST *ast) = 0;
 
-    virtual void gen_variable_decl(VariableDeclAST *ast) = 0;
+    virtual Value gen_variable_decl(VariableDeclAST *ast) = 0;
 };
 
 /// 输出代码的 visitor
@@ -84,43 +76,34 @@ class ADispatcher {
 /// 访问的规则**不在此定义**，而是在 data 里。
 class ASTDispatcher : public ADispatcher {
    public:
-    void gen_global_begin(GlobalAST *ast) override;
-    void gen_global_end(GlobalAST *ast) override;
+    Value gen_global(GlobalAST *ast) override;
 
-    void gen_array_type_decl(ArrayTypeDeclAST *ast) override;
-    void gen_basic_type(BasicTypeAST *ast) override;
-    void gen_pointer_type_decl(PointerTypeDeclAST *ast) override;
-    void gen_type_def(TypeDefAST *ast) override;
+    Value gen_array_type_decl(ArrayTypeDeclAST *ast) override;
+    Value gen_basic_type(BasicTypeAST *ast) override;
+    Value gen_pointer_type_decl(PointerTypeDeclAST *ast) override;
+    Value gen_type_def(TypeDefAST *ast) override;
 
-    void gen_number_expr(NumberExprAST *ast) override;
-    void gen_string_expr(StringExprAST *ast) override;
-    void gen_char_expr(CharExprAST *ast) override;
-    void gen_variable_expr(VariableExprAST *ast) override;
-    void gen_return(ReturnAST *ast) override;
-    void gen_unary_expr(UnaryExprAST *ast) override;
-    void gen_binary_expr(BinaryExprAST *ast) override;
-    void gen_call_expr(CallExprAST *ast) override;
+    Value gen_number_expr(NumberExprAST *ast) override;
+    Value gen_string_expr(StringExprAST *ast) override;
+    Value gen_char_expr(CharExprAST *ast) override;
+    Value gen_variable_expr(VariableExprAST *ast) override;
+    Value gen_return(ReturnAST *ast) override;
+    Value gen_unary_expr(UnaryExprAST *ast) override;
+    Value gen_binary_expr(BinaryExprAST *ast) override;
+    Value gen_call_expr(CallExprAST *ast) override;
 
-    /// 只生产if，不包含block
-    void gen_if_statement_begin(IfStatementAST *ast) override;
-    void gen_if_statement_else(IfStatementAST *ast) override;
-    void gen_if_statement_end(IfStatementAST *ast) override;
-    /// 只生产while，不包含block
-    void gen_while_statement_begin(WhileStatementAST *ast) override;
-    void gen_while_statement_end(WhileStatementAST *ast) override;
-    /// 只生产for，不包含block
-    void gen_for_statement_begin(ForStatementAST *ast) override;
-    void gen_for_statement_end(ForStatementAST *ast) override;
+    Value gen_if_statement(IfStatementAST *ast) override;
+    Value gen_while_statement(WhileStatementAST *ast) override;
+    Value gen_for_statement(ForStatementAST *ast) override;
 
-    void gen_function(FunctionAST *ast) override;
-    void gen_function_signature(FunctionSignatureAST *ast) override;
+    Value gen_function(FunctionAST *ast) override;
+    Value gen_function_signature(FunctionSignatureAST *ast) override;
 
-    void gen_struct(StructDeclAST *ast) override;
+    Value gen_struct(StructDeclAST *ast) override;
 
-    void gen_block_begin(BlockAST *ast) override;
-    void gen_block_end(BlockAST *ast) override;
+    Value gen_block(BlockAST *ast) override;
 
-    void gen_variable_decl(VariableDeclAST *ast) override;
+    Value gen_variable_decl(VariableDeclAST *ast) override;
 };
 
 enum PlaceHolder { PLACE_VOID, PLACE_BEGIN = 1, PLACE_END = 2 };
@@ -172,38 +155,31 @@ class VisualDispatcher : public ADispatcher {
 
    public:
     VisualDispatcher(std::ostream &log) : log(log) {}
-    void gen_global_begin(GlobalAST *ast) override;
-    void gen_global_end(GlobalAST *ast) override;
+    Value gen_global(GlobalAST *ast) override;
 
-    void gen_array_type_decl(ArrayTypeDeclAST *ast) override;
-    void gen_basic_type(BasicTypeAST *ast) override;
-    void gen_pointer_type_decl(PointerTypeDeclAST *ast) override;
+    Value gen_array_type_decl(ArrayTypeDeclAST *ast) override;
+    Value gen_basic_type(BasicTypeAST *ast) override;
+    Value gen_pointer_type_decl(PointerTypeDeclAST *ast) override;
 
-    void gen_number_expr(NumberExprAST *ast) override;
-    void gen_string_expr(StringExprAST *ast) override;
-    void gen_variable_expr(VariableExprAST *ast) override;
-    void gen_return(ReturnAST *ast) override;
-    void gen_unary_expr(UnaryExprAST *ast) override;
-    void gen_binary_expr(BinaryExprAST *ast) override;
-    void gen_call_expr(CallExprAST *ast) override;
+    Value gen_number_expr(NumberExprAST *ast) override;
+    Value gen_string_expr(StringExprAST *ast) override;
+    Value gen_variable_expr(VariableExprAST *ast) override;
+    Value gen_return(ReturnAST *ast) override;
+    Value gen_unary_expr(UnaryExprAST *ast) override;
+    Value gen_binary_expr(BinaryExprAST *ast) override;
+    Value gen_call_expr(CallExprAST *ast) override;
 
-    /// 只生产if，不包含block
-    void gen_if_statement_begin(IfStatementAST *ast) override;
-    void gen_if_statement_end(IfStatementAST *ast) override;
-    /// 只生产while，不包含block
-    void gen_while_statement_begin(WhileStatementAST *ast) override;
-    void gen_while_statement_end(WhileStatementAST *ast) override;
-    /// 只生产for，不包含block
-    void gen_for_statement_begin(ForStatementAST *ast) override;
-    void gen_for_statement_end(ForStatementAST *ast) override;
+    Value gen_if_statement(IfStatementAST *ast) override;
 
-    void gen_function(FunctionAST *ast) override;
-    void gen_function_signature(FunctionSignatureAST *ast) override;
+    Value gen_while_statement(WhileStatementAST *ast) override;
+    Value gen_for_statement(ForStatementAST *ast) override;
 
-    void gen_struct(StructDeclAST *ast) override;
+    Value gen_function(FunctionAST *ast) override;
+    Value gen_function_signature(FunctionSignatureAST *ast) override;
 
-    void gen_block_begin(BlockAST *ast) override;
-    void gen_block_end(BlockAST *ast) override;
+    Value gen_struct(StructDeclAST *ast) override;
 
-    void gen_variable_decl(VariableDeclAST *ast) override;
+    Value gen_block(BlockAST *ast) override;
+
+    Value gen_variable_decl(VariableDeclAST *ast) override;
 };
